5cf8b9b5262ede81c9701419ac86247a87ea5e51
FelipeAraujo32 <felipecafsx@gmail.com>
2024-05-10 16:11:55 -0300

    config

--- bank_santander/src/test/java/com/felipearaujo/bank_santander/BankSantanderApplicationTests.java
+++ bank_santander/src/test/java/com/felipearaujo/bank_santander/BankSantanderApplicationTests.java
@@ -1,13 +1,7 @@
 package com.felipearaujo.bank_santander;
-
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BankSantanderApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
